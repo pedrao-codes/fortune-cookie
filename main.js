43196bf9a650a1ddf9b39277d1fadba97c2fc624
Pedro Rocha <82539477+pedrao-codes@users.noreply.github.com>
2023-08-01 20:10:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const cookie = document.querySelector('#fortune-cookie')
+const btnReset = document.querySelector('#btn-reset')
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const phraseParagraph = document.querySelector('#phrase')
+const phrases = [
+    'A vida trará coisas boas se tiver paciência.',
+    'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
+    'Não compense na ira o que lhe falta na razão.',
+    'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+    'A maior de todas as torres começa no solo.',
+    'Não há que ser forte. Há que ser flexível.',
+    'Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?',
+    'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
+    'A juventude não é uma época da vida, é um estado de espírito.',
+    'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.'
+]
+
+cookie.addEventListener('click', rafflePhrase)
+btnReset.addEventListener('click', changeScreen)
+
+function rafflePhrase() {
+    let phrasesIndex = phrases.length
+    let phraseDrawn = (Math.random() * (phrasesIndex - 1)).toFixed(0)
+
+    phraseParagraph.textContent = phrases[phraseDrawn]
+
+    changeScreen()
+}
+
+function changeScreen() {
+    screen1.classList.toggle('hide')
+    screen2.classList.toggle('hide')
+}
